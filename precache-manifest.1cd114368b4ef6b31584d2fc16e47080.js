5e2a1927a6e9ebf908e20c2084914e656cf019ce
gopla <yudhistn@gmail.com>
2020-07-08 20:59:04 +0700

    Updates

---
+++ precache-manifest.1cd114368b4ef6b31584d2fc16e47080.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462068bfef0fedd54bb31d5935573a90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d298182214f491822996",
+    "url": "/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "5647036b26520a294a54",
+    "url": "/static/css/main.713e46c3.chunk.css"
+  },
+  {
+    "revision": "d298182214f491822996",
+    "url": "/static/js/2.e06611df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e06611df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5647036b26520a294a54",
+    "url": "/static/js/main.1d7fa7a1.chunk.js"
+  },
+  {
+    "revision": "6563055f66d5bd928e4f",
+    "url": "/static/js/runtime-main.3ab6d52f.js"
+  }
+]);
